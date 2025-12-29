e1196de1a1efc5786d44a1c9bddde09a014b968d
Roch Bajracharya <rochbajracharya@sabba807.corp.sabbashop.com>
2025-12-29 16:26:29 -0500

    Implemented map

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,10 @@ import styles from "./Button.module.css";
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func,
-  type: PropTypes.oneOf(["primary", "back"]).isRequired,
+  type: PropTypes.oneOf(["primary", "back", "position"]).isRequired,
 };
 
 function Button({ children, onClick, type }) {
-
   return (
     <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
       {children}
